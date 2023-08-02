85a96c52d6a6b61a6d458782b71ce64e3e222c2a
DavidHODs <davidoluwatobi41@gmail.com>
2023-08-02 12:19:49 +0100

    utility function GetUserData validates verification request and returns data of verified google user

--- go.mod
+++ go.mod
@@ -2,11 +2,14 @@ module github.com/DavidHODs/Google-OAuth
 
 go 1.20
 
+require github.com/gorilla/sessions v1.2.1
+
 require (
 	cloud.google.com/go/compute v1.20.1 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/oauth2 v0.10.0 // indirect
--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,12 @@
 package utils
 
 import (
+	"context"
 	"crypto/rand"
+	"errors"
+	"io"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gorilla/sessions"
@@ -19,6 +23,13 @@ const (
 	tokenSet    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	tokenLength = 15
 )
+
+type OAuthData struct {
+	Id             string `json:"id"`
+	Email          string `json:"email"`
+	Verified_email bool   `json:"verified_email"`
+	Picture        string `json:"picture"`
+}
 
 // LoadFile returns the value of a specified key from .env file
 func LoadFile(key string) string {
@@ -45,4 +56,34 @@ func TokenString() (string, error) {
 	}
 
 	return string(randomBytes), nil
+}
+
+// GetUserData validates verification request and returns data of verified google user
+func GetUserData(state, code, tokenCode string) ([]byte, error) {
+	// compares the generated token string to the token retrieved from the parsed URL
+	if state != tokenCode {
+		return nil, errors.New("invalid user")
+	}
+
+	// converts authorization code into a token
+	token, err := OAuthgolang.Exchange(context.Background(), code)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	// this is done to prevent memory leakage
+	defer response.Body.Close()
+
+	data, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// returns data of verified google user
+	return data, nil
 }
