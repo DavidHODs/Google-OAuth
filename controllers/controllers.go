fddfeda516f011980fcfa4cbbd4b6fd4cb00445d
DavidHODs <davidoluwatobi41@gmail.com>
2023-08-02 12:14:04 +0100

    update init function to load env variables

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -6,11 +6,12 @@ import (
 	"golang.org/x/oauth2/google"
 )
 
+// init loads the neccessary configuration details required by oauth2 package.
 func init() {
 	utils.OAuthgolang = &oauth2.Config{
-		RedirectURL:  "",
-		ClientID:     "",
-		ClientSecret: "",
+		RedirectURL:  utils.LoadFile("REDIRECT_URL"),
+		ClientID:     utils.LoadFile("CLIENT_ID"),
+		ClientSecret: utils.LoadFile("CLIENT_SECRET"),
 		// scopes limits the access given to a token. this scope returns just the user info of the
 		// signed in email address
 		Scopes:   []string{"https://www.googleapis.com/auth/userinfo.email"},
