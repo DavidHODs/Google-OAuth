fadcb91bb0a3d38ccbb266934eb625be897492cc
DavidHODs <davidoluwatobi41@gmail.com>
2023-08-02 12:13:18 +0100

    setup utility function to load env file

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/oauth2 v0.10.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,21 @@
 package utils
 
-import "golang.org/x/oauth2"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+)
 
 var OAuthgolang *oauth2.Config
+
+// LoadFile returns the value of a specified key from .env file
+func LoadFile(key string) string {
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
